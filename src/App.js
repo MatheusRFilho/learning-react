35ae064ff0b6f0867c7b5e0e765e6662bfe2acc4
Matheus Rodrigo <matheus.rodrigo@In8.com.br>
2023-03-16 14:18:13 -0300

    lifeCycle

--- src/App.js
+++ src/App.js
@@ -1,47 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
   state = {
-    name: 'Matheus Filho',
     counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O Titulo 1',
+        body: 'O Corpo 1',
+      },
+      {
+        id: 2,
+        title: 'O Titulo 2',
+        body: 'O Corpo 2',
+      },
+      {
+        id: 3,
+        title: 'O Titulo 3',
+        body: 'O Corpo 3',
+      },
+    ],
   };
+  timeOutUpdate = null;
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
+  componentDidMount() {
+    this.handleTimeout();
+  }
 
-    this.setState({
-      counter: counter + 1,
-    });
-  };
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
 
-  handlePClick = () => {
-    this.setState({
-      name: 'Matheus Rodrigo',
-    });
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O titulo mudou';
+
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({
+        posts,
+        counter: counter + 1,
+      });
+    }, 1000);
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts, counter } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Add Counter
-          </a>
-        </header>
+        <h1>{counter}</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
